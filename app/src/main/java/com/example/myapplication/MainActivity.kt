c7bd673c85e88a8f9890c1f488242e104fed50af
Norvy <71366308+Rattonikus@users.noreply.github.com>
2024-04-13 13:06:28 -0600

    Successful recreation of first swiftUI app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,12 +19,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -30,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity()
 {
@@ -38,9 +46,9 @@ class MainActivity : ComponentActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContent()
-        {
-            Greeting(name = "name")
+        setContent {
+            colorClicker()
+           // Greeting(name = "name")
         }
     }
 
@@ -67,9 +75,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier)
 @Composable
 fun testRow()
 {
-    Row{
-        Text(text = "h")
-        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+    Box(Modifier.background(Color.Cyan))
+    {
+        Row{
+            Text(text = "h")
+            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+        }
+    }
+
+}
+
+@Composable
+fun colorClicker(modifier: Modifier = Modifier)
+{
+    var color by remember {
+        mutableStateOf(Color.Cyan)
+    }
+
+    Box(modifier = modifier
+        .background(color)
+        .fillMaxSize())
+    {
+        Row(modifier = modifier
+            .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center)
+        {
+            Icon(imageVector = Icons.Default.CheckCircle, contentDescription = null)
+            Text(text = "Click me!",
+                 color = Color.White,
+                 modifier = modifier
+                     .clickable {
+                         color = Color(
+                                 Random.nextFloat(),
+                                 Random.nextFloat(),
+                                 Random.nextFloat()
+                         )
+                     })
+        }
+
     }
 }
 
